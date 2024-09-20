88b3f4ba03c19229e1e624e0e737fdd298e3b745
TheXeen <160883485+TheXeen@users.noreply.github.com>
2024-09-20 10:29:07 -0700

    Update serializers.py

--- mycinema/cinema/serializers.py
+++ mycinema/cinema/serializers.py
@@ -1,19 +1,16 @@
 from rest_framework import serializers
 from .models import Movie, Hall, Session
 
-# Сериализатор для модели фильмов
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'title', 'description', 'release_date', 'poster']
 
-# Сериализатор для модели залов
 class HallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hall
         fields = ['id', 'name', 'size']  # Используйте 'size', если это правильное поле
 
-# Сериализатор для модели сеансов
 class SessionSerializer(serializers.ModelSerializer):
     movie = serializers.PrimaryKeyRelatedField(queryset=Movie.objects.all())
     hall = serializers.PrimaryKeyRelatedField(queryset=Hall.objects.all())
